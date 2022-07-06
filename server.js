b7fa4df423edcf06a833a5d21fca1e0af0ef345a
MichaelAharoni <michaelaharoni001@gmail.com>
2022-07-06 15:03:35 +0300

    angular deploy

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const { setupSocketAPI } = require('./services/socket.service')
+const http = require('http').createServer(app) /* surrounding http let us use WebSockets */
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000/', 'http://localhost:3000/','https://127.0.0.1:3000/', 'https://localhost:3000/'],
+        credentials: true
+    }
+    app.use(bodyParser.json())
+    app.use(cors({}))
+}
+setupSocketAPI(http)
+
+
+http.listen(3030,()=>{console.log('Server is running on port 3030')})
---
+++ services/socket.service.js
@@ -0,0 +1,25 @@
+var gIo = null
+
+function setupSocketAPI(http) {
+    gIo = require('socket.io')(http)
+    gIo.on('connection', socket => {
+        socket.on('join-room', (roomId, userId) => {
+            socket.join(roomId)
+        })
+        socket.on('check-socket', (str) => {
+            socket.emit('check-socket', str)
+        })
+        socket.on('disconnect', socket => {
+        })
+    })
+}
+
+function emitTo({ type, data, label }) {
+    if (label) gIo.to('watching:' + label).emit(type, data)
+    else gIo.emit(type, data)
+}
+
+module.exports = {
+    setupSocketAPI,
+    emitTo
+}
