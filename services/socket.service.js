21b415c528e753ce0d3dd08d23d3524313e93ed7
MichaelAharoni <michaelaharoni001@gmail.com>
2022-07-06 21:47:44 +0300

    ssss

--- services/socket.service.js
+++ services/socket.service.js
@@ -5,11 +5,16 @@ function setupSocketAPI(http) {
     gIo.on('connection', socket => {
         socket.on('join-room', (roomId) => {
             socket.join(roomId)
+            socket.room = roomId // BAD PRACTICE !
             console.log('JOINED ROOM '+roomId)
         })
-        socket.on('check-socket', (str) => {
-            socket.emit('check-socket', str)
+        socket.on('store-candidate',(data)=>{
+
+            gIo.to(socket.room).emit('got-candidate',data)
         })
+        socket.on('store-offer',(data)=>{gIo.to(socket.room).emit('got-offer',data)})
+        socket.on('store-answer',(data)=>{gIo.to(socket.room).emit('got-answer',data)})
+        socket.on('join-call',()=>{gIo.to(socket.room).emit('new-call')})
         socket.on('disconnect', socket => {
         })
     })
